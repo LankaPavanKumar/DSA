c2c81e1ea2d172eca7e902a7c61365d9a7895942
LankaPavanKumar <142505822+LankaPavanKumar@users.noreply.github.com>
2023-10-21 23:02:56 +0530

    Add files via upload

---
+++ Pavan.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Pavan
+{
+	public static void main(String[] args)
+	{
+		jFrame f= new jFrame();
+		jButton b=new jButton();
+		b.setBounds(130,100,100,40);
+		f.add(b);
+		f.setSize(400,500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+}
